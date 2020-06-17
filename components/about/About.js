c399d2728bcf1a64fc7afd1306becc491890717e
hoelzlmanuel <hoelzl1912@gmail.com>
2020-06-17 20:38:01 +0200

    update about and trivia

--- components/about/About.js
+++ components/about/About.js
@@ -7,7 +7,14 @@ const About = (props) => (
     <div className="text-lg my-4">
       my name is <b>manuel hölzl</b>, and i am a student at the informatics department of the <b>htl st. pölten</b> in austria with a focus on java, relational database design, network engineering, business economics and cyber security.
     </div>
-    <h2 className="text-3xl font-extrabold">what do i do?</h2>
+    
+  </div>
+)
+
+export default About
+
+/*
+<h2 className="text-3xl font-extrabold">what do i do?</h2>
     <div className="text-lg my-4">
       i like to spend my free time <b>developing</b> &mdash; be it a new webpage (like this one), an autonomous robot for a competition or learning how to make the web more secure. i'm not a tech-only guy, however: i also play the piano and am a board member of a <b>non-profit organization</b> i co-founded in november 2018 called <span className="italic">association for technology, research and innovation austria</span> (or <a className="font-bold link link-black" href="https://atria.or.at/">atria</a>
           <embed className="ml-1 inline align-middle mb-1" src="images/external-link.svg" /> for short). we founded it with the goal of creating an environment where knowledge can be shared and ideas can be realised.
@@ -18,7 +25,4 @@ const About = (props) => (
       <div className="my-4" />
       because i am incredibly passionate about this, i am always looking for ways to <b>share what i've learnt</b> and help others achieve their goals. one way i am doing this is through the association i co-founded &mdash; atria &mdash; where we help each other with our projects, host workshops or participate in various competitions &mdash; together.
     </div>
-  </div>
-)
-
-export default About
+*/
